f4d87e60e1eebc5c43d5213dd41994bd7f236e1f
Hojun Park <hopark@c10r2s14.42seoul.kr>
2021-06-05 20:12:08 +0900

    first

---
+++ boot.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+PYTHON_PATH="/usr/bin/python3"
+VEN_DIR="venv_rush01"
+
+# setup venv
+$PYTHON_PATH -m venv $VEN_DIR
+source $VEN_DIR/bin/activate
+
+# python version
+python -m pip --version
+
+# pip upgrade
+pip install --upgrade pip
+
+# pip install 
+# python -m pip install -q -I django psycopg2-binary
+
+
+# nginx install
+brew install nginx
+
+
+# make requirement.txt
+pip freeze > requirement.txt
+
+# sever start
+# django-admin startproject boot
+# python3 manage.py startapp ex00
+# python3 manage.py makemigrations
+# python3 manage.py migrate
+# python3 manage.py runserver
+
+
+# brew services start nginx
+
+#error
+# pip install --upgrade --force-reinstall Django
+
+# migrations del
+# find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+# find . -path "*/migrations/*.pyc"  -delete
+
