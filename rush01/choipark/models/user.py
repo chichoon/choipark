cf81e553cf69de387d942561e768f01c28a18f2d
chichoon <chichoon.choi@gmail.com>
2021-06-07 13:23:33 +0900

    profile finish

--- rush01/choipark/models/user.py
+++ rush01/choipark/models/user.py
@@ -2,23 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 
-
-# Create your models here.
-class User(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField('username', unique=True)
+class UserProfileModel(models.Model):
+    username = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField('name', max_length=30)
     surname = models.CharField('surname', max_length=30)
-    email = models.EmailField('email', unique=True)
-    description = models.TextField('description')
-    profile_image = models.ImageField('profile_image')
-
-    objects = UserManager()
-
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['username']
-
-    class Meta:
-        verbose_name = _('user')
-        verbose_name_plural = _('users')
-        swappable = 'AUTH_USER_MODEL'
+    email = models.EmailField('email', blank=True)
+    description = models.TextField('description', blank=True)
+    profile_image = models.ImageField('profile_image', blank=True)
